a6ed62358957b1f5c01cd06f76af1e0f55cca116
Stefankar1000 <stefankar1000@gmail.com>
2020-02-12 21:20:11 +0100

    Info command.
    Various stats about the host system and library versions.

--- nagatoro/cogs/management.py
+++ nagatoro/cogs/management.py
@@ -1,5 +1,9 @@
 import os
+import platform
+from psutil import virtual_memory
+from cpuinfo import get_cpu_info
 from time import time
+from discord import __version__ as discord_version
 from datetime import timedelta
 from discord import Color
 from discord.ext.commands import Cog, Context, Bot, command, group, is_owner, \
@@ -8,6 +12,13 @@ from discord.ext.tasks import loop
 
 from nagatoro.objects import Embed
 from nagatoro.utils.db import get_prefix, set_prefix
+
+
+def get_size(bytes: int):
+    for unit in ["", "K", "M", "G", "T", "P"]:
+        if bytes < 1024:
+            return f"{bytes:.2f}{unit}"
+        bytes /= 1024
 
 
 class Management(Cog, command_attrs=dict(ignore_extra=True)):
@@ -31,18 +42,17 @@ class Management(Cog, command_attrs=dict(ignore_extra=True)):
             except ExtensionAlreadyLoaded:
                 pass
 
-    @staticmethod
-    def unload_cogs(bot: Bot):
-        for extension in list(bot.extensions):
-            bot.unload_extension(extension)
-
     @command(name="reload", aliases=["r"], hidden=True)
     @is_owner()
     async def reload(self, ctx: Context):
         """Reload all cogs and commands"""
 
-        self.unload_cogs(self.bot)
-        self.load_cogs(self.bot)
+        for extension in list(self.bot.extensions):
+            try:
+                self.bot.reload_extension(extension)
+            except ExtensionAlreadyLoaded:
+                pass
+
         await ctx.send(f"Reloaded **{len(self.bot.commands)}** commands.")
 
     @command(name="ping")
@@ -51,8 +61,10 @@ class Management(Cog, command_attrs=dict(ignore_extra=True)):
 
         embed = Embed(ctx, title="Ping")
         ping = round(self.bot.latency * 1000)
+
         embed.description = f":ping_pong:‎‎{ping}ms"
         await ctx.send(embed=embed)
+        return ping
 
     @command(name="uptime")
     async def uptime(self, ctx: Context):
@@ -63,6 +75,37 @@ class Management(Cog, command_attrs=dict(ignore_extra=True)):
                                      self.bot.start_timestamp)
         uptime = timedelta(seconds=timestamp_difference)
         embed = Embed(ctx, title="Uptime", description=str(uptime))
+
+        await ctx.send(embed=embed)
+        return uptime
+
+    @command(name="info")
+    async def info(self, ctx: Context):
+        """Bot info"""
+        git_branch = os.popen(
+            "git branch | awk '{print $2}' | sed '/^\s*$/d'").read()
+
+        embed = Embed(ctx, title="Bot info", color=Color(0x56517b))
+        if self.bot.config.testing:
+            embed.description = f"Development version, git: **{git_branch}**"
+        embed.set_thumbnail(url=self.bot.user.avatar_url)
+
+        embed.add_fields(
+            ("Ping", f"{round(self.bot.latency * 1000)}ms"),
+            ("Uptime",
+             str(timedelta(seconds=round(time() - self.bot.start_timestamp)))),
+            ("Commands", f"{len(self.bot.commands)} commands"),
+            ("Creator", str(await self.bot.fetch_user(self.bot.owner_id))),
+            ("Library", f"discord.py {discord_version}"),
+            ("Python version",
+             f"{platform.python_implementation()} {platform.python_version()}"),
+            ("System", f"{platform.system()} {platform.release()}"),
+            ("Processor", get_cpu_info()["brand"]),
+            ("Memory",
+             f"{get_size(virtual_memory().used)}/"
+             f"{get_size(virtual_memory().total)} "
+             f"({virtual_memory().percent}%)")
+        )
 
         await ctx.send(embed=embed)
 
