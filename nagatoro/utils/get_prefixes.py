eadbcd416048865620f72b8711fc040c5574cdd0
Stefankar1000 <stefankar1000@gmail.com>
2020-02-12 15:53:36 +0100

    Actually use prefix from the config.
    Oops forgot to add that earlier.

--- nagatoro/utils/get_prefixes.py
+++ nagatoro/utils/get_prefixes.py
@@ -5,7 +5,7 @@ from nagatoro.utils.db import get_prefix
 
 
 async def get_prefixes(bot: commands.Bot, message: Message):
-    prefixes = [";"]
+    prefixes = [bot.config.prefix]
     # TODO: Make prefix caching, so we don't constantly use the database
     if prefix := await get_prefix(message.guild.id):
         prefixes.append(prefix)
