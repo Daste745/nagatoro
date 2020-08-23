08d7ddd5223e6b99db6623ea1c4c945309f6c739
Stefankar1000 <stefankar1000@gmail.com>
2020-08-23 19:59:16 +0200

    Clean up type hints in get_prefixes.

--- nagatoro/utils/get_prefixes.py
+++ nagatoro/utils/get_prefixes.py
@@ -1,17 +1,19 @@
-from discord.ext import commands
 from discord import Message, DMChannel
+from discord.ext.commands import when_mentioned_or
 
+from nagatoro import Bot
 from nagatoro.utils.db import get_prefix
 
 
-async def get_prefixes(bot: commands.Bot, message: Message):
+async def get_prefixes(bot: Bot, message: Message):
     prefixes = [bot.config.prefix]
-    # TODO: Make prefix caching, so we don't constantly use the database.
-    if not isinstance(message.channel, DMChannel) \
-            and not bot.config.testing \
-            and (prefix := await get_prefix(message.guild.id)):
+    if (
+        not isinstance(message.channel, DMChannel)
+        and not bot.config.testing
+        and (prefix := await get_prefix(message.guild.id))
+    ):
         # 'not bot.config.testing' prevents double messages when running
         # a development instance of the bot.
         prefixes.append(prefix)
 
-    return commands.when_mentioned_or(*prefixes)(bot, message)
+    return when_mentioned_or(*prefixes)(bot, message)
