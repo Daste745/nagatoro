9afa038a253322d7beb0ac7cb9f95294500ac680
Stefankar1000 <stefankar1000@gmail.com>
2020-09-04 19:43:36 +0200

    Remove cyclic import from get_prefixes.

--- nagatoro/utils/get_prefixes.py
+++ nagatoro/utils/get_prefixes.py
@@ -1,11 +1,10 @@
 from discord import Message, DMChannel
 from discord.ext.commands import when_mentioned_or
 
-from nagatoro import Bot
 from nagatoro.utils.db import get_prefix
 
 
-async def get_prefixes(bot: Bot, message: Message):
+async def get_prefixes(bot, message: Message):
     prefixes = [bot.config.prefix]
     if (
         not isinstance(message.channel, DMChannel)
