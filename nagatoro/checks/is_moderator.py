bb6ea2f4295a8eb630d186e273c7e9c9ffa0048f
Stefankar1000 <stefankar1000@gmail.com>
2020-09-09 07:48:07 +0200

    Port moderation module to tortoise-orm.

--- nagatoro/checks/is_moderator.py
+++ nagatoro/checks/is_moderator.py
@@ -1,25 +1,21 @@
-from pony.orm import db_session
-from discord import utils
 from discord.ext.commands import Context, check
 from discord.ext.commands.errors import MissingRole, CommandError
 
-from nagatoro.utils.db import get_guild, get_mod_role
+from nagatoro.db import Guild
 
 
 def is_moderator():
     async def predicate(ctx: Context):
-        with db_session:
-            guild = await get_guild(ctx.guild.id)
+        guild, _ = await Guild.get_or_create(id=ctx.guild.id)
 
-            if guild.mod_role not in [i.id for i in ctx.author.roles] \
-                    or not guild.mod_role:
-                mod_role = await get_mod_role(ctx.bot, ctx.guild.id)
+        if guild.moderator_role not in [i.id for i in ctx.author.roles]:
+            moderator_role = ctx.guild.get_role(guild.moderator_role)
 
-                if not mod_role:
-                    raise CommandError("Mod role was not set for this server.")
-                else:
-                    raise MissingRole(mod_role.name)
+            if not moderator_role:
+                raise CommandError("Mod role was not set for this server.")
             else:
-                return True
+                raise MissingRole(moderator_role.name)
+        else:
+            return True
 
     return check(predicate)
