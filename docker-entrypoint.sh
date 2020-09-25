09cee6991205e56b3390a17b18f39013ad6db585
mlunax <lunax@mlunax.com>
2020-09-25 19:20:47 +0200

    start use .env

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 if [ ! -d "/app/data/action_config.json" ]; then
     cp /app/data-example/action_config.json /app/data -v
-    echo 1
-fi
-if [ ! -d "/app/data/config.json" ] || [ ! -d "/app/data/config.example.json" ]; then
-    cp /app/data-example/config.example.json /app/data -v
-    echo 2
 fi
 cd /app
-echo 0
 python3 -u nagatoro.py
