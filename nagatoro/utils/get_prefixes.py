80323a03fb53a6bd380a168e85b90c5fac955363
Daste <stefankar1000@gmail.com>
2021-01-02 16:39:01 +0100

    Disable default prefix if custom prefix is set

--- nagatoro/utils/get_prefixes.py
+++ nagatoro/utils/get_prefixes.py
@@ -1,24 +1,12 @@
 from discord.ext.commands import when_mentioned_or, when_mentioned
-from asyncio import TimeoutError
 
 from nagatoro.db import Guild
 
 
 async def get_prefixes(bot, message):
-    prefixes = []
+    guild, _ = await Guild.get_or_create(id=message.guild.id)
 
-    if prefix := bot.config.prefix:
-        prefixes.append(prefix)
-
-    if message.guild:
-        try:
-            guild, _ = await Guild.get_or_create(id=message.guild.id)
-            if guild.prefix:
-                prefixes.append(guild.prefix)
-        except TimeoutError:
-            pass
-
-    if prefixes:
-        return when_mentioned_or(*prefixes)(bot, message)
+    if bot.config.prefix or (guild and guild.prefix):
+        return when_mentioned_or(guild.prefix or bot.config.prefix)(bot, message)
     else:
         return when_mentioned(bot, message)
