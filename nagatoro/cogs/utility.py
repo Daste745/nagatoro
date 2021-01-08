f5a76b44a1575321d0075359017e4aaf46d22d54
Daste <stefankar1000@gmail.com>
2021-01-08 10:35:38 +0100

    Translations for the utility module

--- nagatoro/cogs/utility.py
+++ nagatoro/cogs/utility.py
@@ -5,6 +5,7 @@ from discord.ext.commands import Cog, Context, command
 
 from nagatoro.converters import Role, User, Member
 from nagatoro.objects import Embed
+from nagatoro.utils import t
 
 
 class Utility(Cog):
@@ -17,21 +18,32 @@ class Utility(Cog):
     async def role(self, ctx: Context, *, role: Role):
         """Shows info about a role"""
 
-        embed = Embed(ctx, title=role.name, color=role.color)
+        embed = Embed(ctx, title=t(ctx, "title", role=role.name), color=role.color)
 
-        embed.add_field(name="ID", value=role.id)
+        embed.add_field(name=t(ctx, "id"), value=role.id)
 
         if len(role.members) > 1:
-            embed.add_field(name="Members", value=str(len(role.members)))
+            embed.add_field(name=t(ctx, "members"), value=str(len(role.members)))
 
-        embed.add_field(name="Mentionable", value="Yes" if role.mentionable else "No")
+        embed.add_field(
+            name=t(ctx, "mentionable"),
+            value=t(ctx, "mentionable_yes")
+            if role.mentionable
+            else t(ctx, "mentionable_no"),
+        )
 
         if role.color != Color.default():
             embed.add_field(
-                name="Color", value=f"{role.color}, rgb{role.color.to_rgb()}"
+                name=t(ctx, "color"),
+                value=t(
+                    ctx,
+                    "color_value",
+                    hex={role.color.value},
+                    rgb=role.color.to_rgb(),
+                ),
             )
 
-        embed.add_field(name="Created at", value=role.created_at)
+        embed.add_field(name=t(ctx, "created_at"), value=role.created_at)
 
         await ctx.send(embed=embed)
 
@@ -42,10 +54,13 @@ class Utility(Cog):
         if not user:
             user = ctx.author
 
-        title = str(user) if not user.bot else f"{user} :robot:"
+        title = str(user) if not user.bot else t(ctx, "title_bot", user=user.name)
         embed = Embed(ctx, title=title, color=user.color)
         embed.set_thumbnail(url=user.avatar_url)
-        embed.add_fields(("ID", user.id), ("Created at", user.created_at))
+        embed.add_fields(
+            (t(ctx, "id"), user.id),
+            (t(ctx, "created_at"), user.created_at),
+        )
 
         await ctx.send(embed=embed)
 
@@ -56,7 +71,7 @@ class Utility(Cog):
         if not user:
             user = ctx.author
 
-        embed = Embed(ctx, title=f"{user.name}'s avatar")
+        embed = Embed(ctx, title=t(ctx, "title", user=user.name))
         embed.set_image(url=user.avatar_url_as(size=2048))
         await ctx.send(embed=embed)
 
@@ -68,12 +83,12 @@ class Utility(Cog):
         embed.set_thumbnail(url=ctx.guild.icon_url_as(size=2048))
 
         embed.add_fields(
-            ("ID", ctx.guild.id),
-            ("Owner", ctx.guild.owner.mention),
-            ("Region", ctx.guild.region),
-            ("Members", str(ctx.guild.member_count)),
-            ("Text channels", str(len(ctx.guild.text_channels))),
-            ("Voice channels", str(len(ctx.guild.voice_channels))),
+            (t(ctx, "id"), ctx.guild.id),
+            (t(ctx, "owner"), ctx.guild.owner.mention),
+            (t(ctx, "region"), ctx.guild.region),
+            (t(ctx, "members"), str(ctx.guild.member_count)),
+            (t(ctx, "text_channels"), str(len(ctx.guild.text_channels))),
+            (t(ctx, "voice_channels"), str(len(ctx.guild.voice_channels))),
         )
 
         await ctx.send(embed=embed)
